04adcf8b047927dcf64bd8d38de79062896bd562
ChuncI-Iunmaru <pwalczak796@gmail.com>
2020-01-06 16:20:51 +0100

    Add auto redirection from home page depending on user role

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,14 +1,26 @@
 import React, { useContext, useEffect } from 'react';
 import authContext from "../../context/auth/authContext";
+import { withRouter } from 'react-router-dom';
 
-const Home = () => {
+const Home = ({history}) => {
     const AuthContext = useContext(authContext);
+
+    const { loadUser, user } = AuthContext;
 
     useEffect(() => {
         console.log('Odświeżam');
-        AuthContext.loadUser();
+        loadUser();
         // eslint-disable-next-line
     }, []);
+
+    useEffect(() => {
+        if (user !== null) {
+            if (user.role === 'student') {
+                history.push("/mySubscriptions")
+            } else history.push("/sets");
+        }
+        // eslint-disable-next-line
+    }, [user, loadUser]);
 
     return (
         <div className="grid-2">
@@ -17,4 +29,4 @@ const Home = () => {
     )
 };
 
-export default Home;
+export default withRouter(Home);
